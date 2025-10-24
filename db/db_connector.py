6e8f0a36bf5dc8e9e7cec69c7e4613381b6f979d
Marcos V. Monteiro <98278090+MontMarcos@users.noreply.github.com>
2025-10-24 12:45:09 -0300

    Atualizando a lista

--- db/db_connector.py
+++ db/db_connector.py
@@ -15,7 +15,7 @@ class GiftRepository:
   
     INITIAL_GIFTS = [
         ("Escorredor de Pratos", False), ("Cafeteira", False), ("Filtro de Barro", False),
-        ("Garrafa de Café", False), ("Jarro de Vidro", False), ("Kit de Xícaras", False),
+        ("Garrafa de Café", False), ("Jarro de Vidro", False), ("Kit de Caneca", False),
         ("Sanduicheira", False), ("Chaleira Elétrica", False), ("Jogo de Copos", False),
         ("Jogo de Colheres de Silicone", False), ("Jogo de Facas", False), ("Jogo de Formas", False),
         ("Jogo de Marinex", False), ("Jogo de Vasilhas", False), ("Jogo de Porta Temperos Multifuncional", False),
